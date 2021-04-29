34a92026a18e2b538dcbe68780ed5a4d0ebc0d9e
Atakan Onol <atakan16@gmail.com>
2021-04-29 22:48:06 +0300

    Creates new Rogue object

--- src/RogueFactory.java
+++ src/RogueFactory.java
@@ -3,6 +3,8 @@ public class RogueFactory extends KnightFactory
     //TODO
     @Override
     public Knight produce(Team team) {
-        return null;
+        Rogue newRogue = new Rogue(team.teamColor, team.getStartingCoordinate(team.teamColor));
+        team.members.add(newRogue);
+        return newRogue;
     }
 }
