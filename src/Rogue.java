e09a8fc5de5081c5177be6016a89f9053b77b7f0
Atakan Onol <atakan16@gmail.com>
2021-04-29 22:48:26 +0300

    Implemented Rogue class

--- src/Rogue.java
+++ src/Rogue.java
@@ -1,4 +1,14 @@
+import java.awt.*;
+
 public class Rogue extends BasicKnight
 {
+    Rogue(Color colorOfTeam, int[] startingCoordinate) {
+
+        super(colorOfTeam, startingCoordinate);
+
+        this.health = 500;
+        this.speed = 100;
+        this.typeOfKnight = "Rogue";
+    }
     //TODO
 }
