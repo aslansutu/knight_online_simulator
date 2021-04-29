ae8b3c3bd569b57f0b14a6596546171b066b2603
Atakan Onol <atakan16@gmail.com>
2021-04-29 22:51:14 +0300

    Gave basic knight properties and methods

--- src/BasicKnight.java
+++ src/BasicKnight.java
@@ -1,4 +1,20 @@
+import java.awt.*;
+
 public class BasicKnight extends Knight
 {
-    //TODO
+    /**
+     * Constructor for a BasicKnight
+     * @param colorOfTeam color of the knight's team
+     * @param startingCoordinate spawn location of knight
+     */
+    BasicKnight(Color colorOfTeam, int[] startingCoordinate){
+        this.setTeamColor(colorOfTeam);
+        this.setNationalPoints(0);
+        this.setTotalDamage(0);
+        this.setKills(0);
+
+        this.setxCoordinate(startingCoordinate[0]);
+        this.setyCoordinate(startingCoordinate[1]);
+    }
+
 }
