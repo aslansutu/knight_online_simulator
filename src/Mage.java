e2e7e15049c031683e859362027ea27464b925ed
Atakan Onol <atakan16@gmail.com>
2021-04-29 22:49:46 +0300

    Implemented mage class

--- src/Mage.java
+++ src/Mage.java
@@ -1,4 +1,14 @@
+import java.awt.*;
+
 public class Mage extends BasicKnight
 {
+    Mage(Color colorOfTeam, int[] startingCoordinate) {
+
+        super(colorOfTeam, startingCoordinate);
+
+        this.health = 400;
+        this.speed = 75;
+        this.typeOfKnight = "Mage";
+    }
     //TODO
 }
