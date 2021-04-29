f4d69385a4ae4fd6ecb0517971d83ae195104ec7
Atakan Onol <atakan16@gmail.com>
2021-04-29 22:50:16 +0300

    Implemented general Knight Decorator

--- src/KnightDecorator.java
+++ src/KnightDecorator.java
@@ -1,4 +1,26 @@
+import java.awt.*;
+import java.io.IOException;
+
 public abstract class KnightDecorator extends Knight
 {
    //TODO
+    protected Knight tempKnight;
+    protected int pieceSize = 20;
+    protected int symbolSize = 5;
+
+    public KnightDecorator(Knight newKnight){
+        tempKnight = newKnight;
+    }
+
+    @Override
+    public void setxCoordinate(int x){ tempKnight.setxCoordinate(x); }
+
+    @Override
+    public void setyCoordinate(int y){ tempKnight.setyCoordinate(y); }
+
+    @Override
+    public int getxCoordinate(){ return tempKnight.getxCoordinate(); }
+
+    @Override
+    public int getyCoordinate(){ return tempKnight.getyCoordinate(); }
 }
