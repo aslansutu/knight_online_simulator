3d60a2defaa6e2c9ac9c2907e5e72a3aa5516f05
Atakan Onol <atakan16@gmail.com>
2021-04-29 23:20:02 +0300

    Updated move function to travel in all directions

--- src/RandomMove.java
+++ src/RandomMove.java
@@ -6,26 +6,26 @@ public class RandomMove extends Strategy
     @Override
     public void act(Knight knight) {
         Random random = new Random();
-        int movementMultiplier = 1;
+        int movementMultiplier = 5;
         int displacement;
 
-        displacement = knight.getxCoordinate() + random.nextInt(knight.speed/movementMultiplier);
-        if (displacement <= 250){
-            displacement = 251;
+        displacement = knight.getxCoordinate() +(random.nextInt(knight.speed * 2) - knight.speed) / movementMultiplier;
+        if (displacement <= 350){
+            displacement = 351;
         }
-        else if (displacement >= 800){
-            displacement = 799;
+        else if (displacement >= 750){
+            displacement = 749;
         }
         knight.setxCoordinate(displacement);
 
 
 
-        displacement = knight.getyCoordinate() + random.nextInt(knight.speed/movementMultiplier);
+        displacement = knight.getyCoordinate() + (random.nextInt(knight.speed * 2) - knight.speed) / movementMultiplier;
         if (displacement <= 0){
             displacement = 1;
         }
-        else if (displacement >= 600){
-            displacement = 599;
+        else if (displacement >= 550){
+            displacement = 549;
         }
         knight.setyCoordinate(displacement);
     }
