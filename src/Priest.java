c526b9ea05fbb571f66ae20901f5149505f8ca80
Atakan Onol <atakan16@gmail.com>
2021-04-29 22:48:56 +0300

    Implemented priest class

--- src/Priest.java
+++ src/Priest.java
@@ -1,4 +1,16 @@
+import java.awt.*;
+
 public class Priest extends BasicKnight
 {
-    //TODO
+
+    Priest(Color colorOfTeam, int[] startingCoordinate){
+
+        super(colorOfTeam, startingCoordinate);
+
+        this.health = 350;
+        this.speed = 50;
+        this.typeOfKnight = "Priest";
+
+    }
+
 }
